f9ba2e408773fb45dac9112180fcfe68887b7dda
Pierre Lemère <pierre.lemere@gmail.com>
2021-01-19 11:05:48 +0100

    .

--- P3/Controller/main.swift
+++ P3/Controller/main.swift
@@ -7,22 +7,4 @@
 
 import Foundation
 
-//MARK: var
-var team1: Team
-var team2: Team
-
-//MARK: Prepare game
-print("\(Text.getText()["start"]!)")
-
-//team one
-print(Text.getText()["createTeam1"]!)
-team1 = Team.createTeam()
-
-//team two
-print(Text.getText()["createTeam2"]!)
-team2 = Team.createTeam()
-
-
-//MARK: Game
-
-Game.fightStart(team1: team1, team2: team2)
+Game.fightStart()
